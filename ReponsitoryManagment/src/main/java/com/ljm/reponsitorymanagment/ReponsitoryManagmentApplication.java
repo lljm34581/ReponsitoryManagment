d6462ca7772f36ace75e548d27b69fc07f732b97
lljm <lljm34581@163.com>
2023-11-22 15:36:19 +0800

    新的仓库管理项目

---
+++ ReponsitoryManagment/src/main/java/com/ljm/reponsitorymanagment/ReponsitoryManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.ljm.reponsitorymanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReponsitoryManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReponsitoryManagmentApplication.class, args);
+    }
+
+}
---
+++ ReponsitoryManagment/src/test/java/com/ljm/reponsitorymanagment/ReponsitoryManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljm.reponsitorymanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReponsitoryManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
